2715e527ab7c9797277141adb64732a6ee27585d
Felix <31962607+RaptorXilef@users.noreply.github.com>
2025-06-22 02:37:50 +0200

    Delete _old-löschen_wenn_fertig directory
    Versehendlich hochgeladen

--- _old-löschen_wenn_fertig/admin/includes/design/_müll/config-menue-pw.php
+++
@@ -1,5 +0,0 @@
-
-<a href="../index.php"><span class="red-text"><strong>Adminbereich</strong></span></a>
-<a href="./">.</a>
-<a href="./">.</a>
-<a href="../change_password.php">Passwort &auml;ndern</a>
