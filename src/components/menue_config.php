47e090735d7f991d76ae8686025039c8d0fb43dc
RaptorXilef <31962607+RaptorXilef@users.noreply.github.com>
2025-07-02 19:02:09 +0200

    fix(navigation): Menülinks dynamisch an Basis-URL angepasst
    Die Links in `menue_config.php` wurden angepasst, um die zuvor definierte `$baseUrl` zu nutzen. Dies behebt Probleme mit der Pfadauflösung auf lokalen Entwicklungsumgebungen (z.B. XAMPP mit Unterordnern) und gewährleistet, dass die Navigation sowohl auf dem Testserver als auch in der Produktivumgebung korrekt funktioniert. Die `$baseUrl` wird dynamisch ermittelt und ermöglicht so absolute Pfade vom Projekt-Root, unabhängig von der Serverkonfiguration. Die Links werden nun mit `htmlspecialchars()` ausgegeben, um die Sicherheit zu erhöhen.

--- src/components/menue_config.php
+++ src/components/menue_config.php
@@ -3,19 +3,29 @@
  * Konfigurationsdatei für das Navigationsmenü.
  * Enthält Links zu den verschiedenen Bereichen der Webseite.
  */
+
+// Stellen Sie sicher, dass $baseUrl hier verfügbar ist.
+// Diese Variable sollte in einer übergeordneten Datei (z.B. header.php) gesetzt werden,
+// die auch die comic_page_renderer.php inkludiert, wo $baseUrl definiert wird.
+if (!isset($baseUrl)) {
+    // Fallback oder Fehlerbehandlung, falls $baseUrl nicht definiert ist.
+    // In einer gut strukturierten Anwendung sollte dies nicht notwendig sein,
+    // da $baseUrl im header.php (via comic_page_renderer.php) definiert wird.
+    error_log("FEHLER: \$baseUrl ist in menue_config.php nicht definiert.");
+    $baseUrl = '/'; // Setze einen Fallback auf den Server-Root, dies könnte aber Fehler verursachen.
+}
 ?>
 <a href="https://www.patreon.com/c/raptorxilef/posts?filters%5Btag%5D=TwoKinds&redirect=true" target="_blank">Patreon</a>
 <br>
-<a href="./">Comic</a>
-<a href="archiv.php">Archiv</a>
+<a href="<?php echo htmlspecialchars($baseUrl); ?>comic/">Comic</a>
+<a href="<?php echo htmlspecialchars($baseUrl); ?>archiv.php">Archiv</a>
 <br>
-<a href="./about.php">Infos</a>
-<a href="./charaktere.php">Charaktere</a>
+<a href="<?php echo htmlspecialchars($baseUrl); ?>about.php">Infos</a>
+<a href="<?php echo htmlspecialchars($baseUrl); ?>charaktere.php">Charaktere</a>
 
-<a href="./faq.php">FAQ</a>
-<a href="./lizenz.php">Lizenz</a>
+<a href="<?php echo htmlspecialchars($baseUrl); ?>faq.php">FAQ</a>
+<a href="<?php echo htmlspecialchars($baseUrl); ?>lizenz.php">Lizenz</a>
 <br>
-<a href="./impressum.php">Impressum</a>
+<a href="<?php echo htmlspecialchars($baseUrl); ?>impressum.php">Impressum</a>
 <br>
-<a href="./newsletter.php"><img src="https://fonts.gstatic.com/s/i/productlogos/gmail_2020q4/v8/web-64dp/logo_gmail_2020q4_color_1x_web_64dp.png" alt="E-Mail-Logo" width="15"> Newsletter <img src="https://ow2.res.office365.com/owalanding/2023.8.17.01/images/favicon.ico?v=4" alt="E-Mail-Logo" width="15"><p><img src="https://www.paypalobjects.com/marketing/web/logos/paypal-mark-color_new.svg" alt="PayPal-Logo" width="15">  Spenden  <img src="https://upload.wikimedia.org/wikipedia/commons/9/94/Patreon_logo.svg" alt="Patreon-Logo" width="15"></a></p>
-
+<a href="<?php echo htmlspecialchars($baseUrl); ?>newsletter.php"><img src="https://fonts.gstatic.com/s/i/productlogos/gmail_2020q4/v8/web-64dp/logo_gmail_2020q4_color_1x_web_64dp.png" alt="E-Mail-Logo" width="15"> Newsletter <img src="https://ow2.res.office365.com/owalanding/2023.8.17.01/images/favicon.ico?v=4" alt="E-Mail-Logo" width="15"><p><img src="https://www.paypalobjects.com/marketing/web/logos/paypal-mark-color_new.svg" alt="PayPal-Logo" width="15">  Spenden  <img src="https://upload.wikimedia.org/wikipedia/commons/9/94/Patreon_logo.svg" alt="Patreon-Logo" width="15"></a></p>
