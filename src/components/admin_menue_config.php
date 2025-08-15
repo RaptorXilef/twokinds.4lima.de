e9435cd415f64b9a6b7a0dfba75ddffba76c14b1
RaptorXilef <31962607+RaptorXilef@users.noreply.github.com>
2025-08-15 22:26:57 +0200

    refactor(menue): Unnötige Elemente im Menü wurden deaktiviert/auskommentiert

--- src/components/admin_menue_config.php
+++ src/components/admin_menue_config.php
@@ -5,27 +5,30 @@
  */
 ?>
 
+
 <div class="sidebar-content">
-    <div class="social">
-        <!-- Soziale Medien Icons -->
-        <div style="display: flex; justify-content: center; gap: 10px; margin-bottom: 10px;">
-            <!-- Patreon Icon -->
-            <a href="https://www.patreon.com/raptorxilef" target="_blank" title="Mein Patreon">
-                <img src="<?php echo htmlspecialchars($baseUrl); ?>assets/icons/patreon.png" alt="Patreon" width="32"
-                    height="32" style="border-radius: 5px;">
-            </a>
-            <!-- InkBunny Icon -->
-            <a href="https://inkbunny.net/RaptorXilefSFW" target="_blank" title="Mein InkBunny">
-                <img src="<?php echo htmlspecialchars($baseUrl); ?>assets/icons/inkbunny.png" alt="InkBunny" width="32"
-                    height="32" style="border-radius: 5px;">
-            </a>
-            <!-- FurAffinity Icon -->
-            <a href="https://paypal.me/RaptorXilef?country.x=DE&locale.x=de_DE" target="_blank" title="Mein Paypal">
-                <img src="<?php echo htmlspecialchars($baseUrl); ?>assets/icons/paypal.png" alt="PayPal" width="32"
-                    height="32" style="border-radius: 5px;">
-            </a>
-        </div>
-    </div>
+    <?php /*
+      *    <div class="social">
+      *        <!-- Soziale Medien Icons -->
+      *        <div style="display: flex; justify-content: center; gap: 10px; margin-bottom: 10px;">
+      *            <!-- Patreon Icon -->
+      *            <a href="https://www.patreon.com/raptorxilef" target="_blank" title="Mein Patreon">
+      *                <img src="<?php echo htmlspecialchars($baseUrl); ?>assets/icons/patreon.png" alt="Patreon" width="32"
+      *                    height="32" style="border-radius: 5px;">
+      *            </a>
+      *            <!-- InkBunny Icon -->
+      *            <a href="https://inkbunny.net/RaptorXilefSFW" target="_blank" title="Mein InkBunny">
+      *                <img src="<?php echo htmlspecialchars($baseUrl); ?>assets/icons/inkbunny.png" alt="InkBunny" width="32"
+      *                    height="32" style="border-radius: 5px;">
+      *            </a>
+      *            <!-- FurAffinity Icon -->
+      *            <a href="https://paypal.me/RaptorXilef?country.x=DE&locale.x=de_DE" target="_blank" title="Mein Paypal">
+      *                <img src="<?php echo htmlspecialchars($baseUrl); ?>assets/icons/paypal.png" alt="PayPal" width="32"
+      *                    height="32" style="border-radius: 5px;">
+      *            </a>
+      *        </div>
+      *    </div>
+      */ ?>
 
     <!-- Menü-Navigation -->
     <nav id="menu" class="menu">
@@ -54,7 +57,8 @@
         <br>
         <br>
         <a id="toggle_lights" class="theme jsdep" href=""><span class="themelabel">Theme</span><span
-                class="themename">LICHT AUS</span></a>
+                class="themename">LICHT
+                AUS</span></a>
     </nav>
     <!-- Menü Ende -->
 </div>
