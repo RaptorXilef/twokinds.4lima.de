14b987745554168752a50a4b4f21a099f2431c04
RaptorXilef <31962607+RaptorXilef@users.noreply.github.com>
2025-09-14 17:27:51 +0200

    fix(general): Behebe kleine Fehler in CSS und JS
    Führt kleine Fehlerbehebungen in den CSS- und JavaScript-Dateien des Projekts durch, um die Stabilität und das Benutzererlebnis zu verbessern.
    
    - Versionsänderung zu: 2.1.0.84

---
+++ src/layout/css/aktualisieren.ps1
@@ -0,0 +1,4 @@
+sass --no-source-map main.scss main.css && npx clean-css-cli --source-map -o main.min.css main.css
+sass --no-source-map main_dark.scss main_dark.css && npx clean-css-cli --source-map -o main_dark.min.css main_dark.css
+sass --no-source-map cookie_banner.scss cookie_banner.css && npx clean-css-cli --source-map -o cookie_banner.min.css cookie_banner.css
+sass --no-source-map cookie_banner_dark.scss cookie_banner_dark.css && npx clean-css-cli --source-map -o cookie_banner_dark.min.css cookie_banner_dark.css
---
+++ src/layout/js/aktualisieren.ps1
@@ -0,0 +1,5 @@
+npx terser archive.js --source-map "filename=archive.min.js.map" --output archive.min.js
+npx terser charaktere.js --source-map "filename=charaktere.min.js.map" --output charaktere.min.js
+npx terser comic.js --source-map "filename=comic.min.js.map" --output comic.min.js
+npx terser common.js --source-map "filename=common.min.js.map" --output common.min.js
+npx terser cookie_consent.js --source-map "filename=cookie_consent.min.js.map" --output cookie_consent.min.js
