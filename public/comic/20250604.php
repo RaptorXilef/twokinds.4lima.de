624a4c167a445fbe4d5c3885104c6d5fa535127c
RaptorXilef <31962607+RaptorXilef@users.noreply.github.com>
2025-10-29 23:34:10 +0100

    fix(comicseiten)

--- public/comic/20250604.php
+++ public/comic/20250604.php
@@ -1,18 +1,1 @@
-<?php
-/**
- * Dies ist eine Comicseite. Sie inkludiert den zentralen Comic-Renderer.
- * Die ID der Comicseite wird automatisch aus dem Dateinamen abgeleitet.
- * 
- * @file      /comic/20250604.php
- * @package   twokinds.4lima.de
- * @author    Felix M. (@RaptorXilef)
- * @copyright 2025 Felix M.
- * @license   Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International <https://github.com/RaptorXilef/twokinds.4lima.de/blob/main/LICENSE>
- * @link      https://github.com/RaptorXilef/twokinds.4lima.de
- * @version   1.0.0
- */
-
-// Inkludiere den zentralen Comic-Renderer.
-// Pfad relativ vom 'comic/' Ordner zum 'src/components/' Ordner.
-require_once __DIR__ . '/../../../src/renderer/renderer_comic_page.php';
-?>
+<?php require_once __DIR__ . '/../../src/renderer/renderer_comic_page.php'; ?>
