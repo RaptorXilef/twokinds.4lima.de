318285a164e95886287037eb3f08a7643fadab91
RaptorXilef <31962607+RaptorXilef@users.noreply.github.com>
2026-07-25 16:54:47 +0200

    fix(ui/core): use config object to retrieve google analytics id
    - Fixed a bug where the Cookie Consent Banner was not rendered because `$settings['google_analytics_id']` was empty. The `TemplateRenderer` only populates a limited `$settings` array for templates.
    - Switched to using the injected `$config->get('google_analytics_id')` method in `_public_footer.phtml` to correctly access the value from `config.php`.
    
    [DE]
    Dieser Commit behebt den Fehler, dass das Cookie-Banner nicht angezeigt wurde. Die Ursache lag darin, dass im Template auf das `$settings`-Array zugegriffen wurde, welches vom `TemplateRenderer` aber nur mit wenigen Basis-Werten (wie der URL) befüllt wird. Die Abfrage nutzt nun korrekt das verfügbare `$config`-Objekt, um die Analytics-ID aus der `config.php` sicher auszulesen.

--- config/config.php
+++ config/config.php
@@ -41,7 +41,7 @@ return [
 
     // --- GRUNDEINSTELLUNGEN RSS und SITEMAP ---
     'base_url'         => 'https://twokinds.4lima.de', // OHNE Slash am Ende!
-    'site_title'       => 'Twokinds - German Translation',
+    'site_title'       => 'Twokinds | Deutsch',
     'site_description' => 'Die deutsche Übersetzung des Webcomics Twokinds von Tom Fischbach, übersetzt von Felix Maywald.',
     'rss_max_items'    => 25,
 
